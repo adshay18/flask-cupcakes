0a1908eb5aa4aa971cd32d4890119154d66b5c60
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-02-07 16:15:45 -0500

    write PATCH and DELETE routes

--- app.py
+++ app.py
@@ -33,3 +33,22 @@ def add_cupcake():
     db.session.commit()
     response = jsonify(cupcake=cupcake.serialize())
     return (response, 201)
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    '''Update cupcake and respond with JSON of the update'''
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+    '''Deletes instance of cupcake'''
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message="deleted")
